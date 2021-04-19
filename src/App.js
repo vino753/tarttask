017771fba8a9ed50b1b4054898f92610114bac74
vino753 <69960684+vino753@users.noreply.github.com>
2021-04-18 22:59:07 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 
-import Table1 from './Table'
+
 import SimpleTabs from './Authentication'
 
 function App() {
